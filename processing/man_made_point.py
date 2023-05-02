c2c0cf20c5569c30cc5aef057d1174b9a6ecbec5
goldyendis <goldyendis@gmail.com>
2023-05-02 16:49:35 +0200

    man_made

--- processing/man_made_point.py
+++ processing/man_made_point.py
@@ -14,4 +14,4 @@ class FeatureClassManMadePoint(AbstractFeatureClass):
         self.duplicate = f"{self.name}_1"
         self.fcgeometry = FeatureClassGeometry(name=self.name, geometry=self.geometry)
         self.fcgeometry.select_by_attribute(attribute="tower", out_name=self.duplicate, inverse=True)
-        tower:str = self.fcgeometry.select_by_attribute(attribute="tower", out_name=f"{self.name}_tower")
+        tower: str = self.fcgeometry.select_by_attribute(attribute="tower", out_name=f"{self.name}_tower")
