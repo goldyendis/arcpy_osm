cd30f0a9b85e9efc80f62d1693147f48a5fb05d4
goldyendis <goldyendis@gmail.com>
2023-05-25 17:05:55 +0200

    highway_line

---
+++ processing/highway_area.py
@@ -0,0 +1,19 @@
+from manipulation.feature_class_geometry import FeatureClassGeometry
+from processing.abstract.feature_process_abstract import AbstractFeatureClass
+
+
+class FeatureClassHighwayArea(AbstractFeatureClass):
+    def __init__(self, feature: str, helper: bool = False) -> None:
+        """
+        Concrete class to process Highway Line feature layer
+        :param feature: str | The name of feature layer
+        """
+        super().__init__()
+        self.name = feature
+        self.geometry = feature.split("_")[-1]
+        self.duplicate = f"{self.name}_1"
+        self.fcgeometry = FeatureClassGeometry(name=self.name, geometry=self.geometry)
+
+
+
+
